1d14e47623c82fd59e47e0c2f6d927adc1710d56
Jex <jex@astra.cloud>
2018-11-25 19:05:48 +0800

    Finish leetcode_problems/107_binary_tree_level_order_traversal_2_test

---
+++ leetcode_problems/107_binary_tree_level_order_traversal_2_test.go
@@ -0,0 +1,68 @@
+package leetcode_problems
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+/* This struct is already defined in 102_binary_tree_level_order_traversal_test.go
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+*/
+
+func TestLevelOrderBottom(t *testing.T) {
+	t.Log("Test 107_binary_tree_level_order_traversal_2_test")
+	list := []struct {
+		Tree TreeNode
+		Ans  [][]int
+	}{
+		{
+			TreeNode{3, &TreeNode{9, nil, nil}, &TreeNode{20, &TreeNode{15, nil, nil}, &TreeNode{7, nil, nil}}},
+			[][]int{[]int{15, 7}, []int{9, 20}, []int{3}},
+		},
+	}
+
+	for _, item := range list {
+		assert.Equal(t, reflect.DeepEqual(levelOrderBottom(&item.Tree), item.Ans), true)
+	}
+}
+
+// BFS
+func levelOrderBottom(root *TreeNode) [][]int {
+	var ans [][]int
+	if root == nil {
+		return ans
+	}
+
+	var nodes []*TreeNode
+	nodes = append(nodes, root)
+	for len(nodes) > 0 {
+		var tmp []int
+		times := len(nodes)
+		for i := 0; i < times; i++ {
+			if nodes[i].Left != nil {
+				nodes = append(nodes, nodes[i].Left)
+			}
+			if nodes[i].Right != nil {
+				nodes = append(nodes, nodes[i].Right)
+			}
+			tmp = append(tmp, nodes[i].Val)
+		}
+		nodes = nodes[times:]
+		ans = append(ans, tmp)
+	}
+
+	return reverseOrder(ans)
+}
+
+func reverseOrder(ans [][]int) [][]int {
+	if len(ans) == 0 {
+		return ans
+	}
+	return append(reverseOrder(ans[1:]), ans[0])
+}
